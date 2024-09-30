bede35505b18c9cab900337dc1b8061c16f458cd
TrueSunGaming <truesungaming@gmail.com>
2024-09-30 13:32:04 +0000

    feat: add substitution to PowerVariable

--- lib/PowerVariable.ts
+++ lib/PowerVariable.ts
@@ -1,6 +1,11 @@
-import type { MaybeComplex } from "./MaybeComplex";
+import { Complex } from "./Complex";
+import { MaybeComplex } from "./MaybeComplex";
 
 export class PowerVariable {
     variable: string;
     power: MaybeComplex;
+
+    substitute(value: MaybeComplex): Complex {
+        return Complex.fromNumber(value)[this.power instanceof Complex ? "pow" : "powReal"](this.power as any);
+    }
 }
